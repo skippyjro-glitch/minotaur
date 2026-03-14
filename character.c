1478b6de866bf4c464997162292e5ad5e9bdd319
skippyjro-glitch <skippyjro@gmail.com>
2026-03-14 19:43:53 -0400

    Fix spacing in check_direction function call
    Removed unnecessary spaces in function call.

--- character.c
+++ character.c
@@ -20,7 +20,7 @@ int move_character(int * y, int * x, char direction, char character) {
     int invalid_direction;
     
     // check for an invalid direction (not LEFT, RIGHT, UP, or DOWN)
-    invalid_direction = check_direction(* x,* y);
+    invalid_direction = check_direction(*x,*y);
     
     // calculate the new coordinates to use on success (store in local variables)
     // check if the new coordinates point to a wallg
