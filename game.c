918b5571e0b0119c8a56d7e00c53a47679d42965
skippyjro-glitch <skippyjro@gmail.com>
2026-03-14 19:06:34 -0400

    Fix win and loss condition checks in game logic

--- game.c
+++ game.c
@@ -10,11 +10,16 @@ extern int width;
 
 int check_win(int player_y, int player_x) {
     // The player wins if they escape the map boundaries (i.e., they are in a place an outer wall tile should be).
+    if (player_y>=height-1||player_x>=width-1||player_y<=0||player_x<=0) {
+        return YOU_WIN;
+    }
     return KEEP_GOING;
 }
 
 int check_loss(int player_y, int player_x, int minotaur_y, int minotaur_x) {
     // The player loses if they are caught by the Minotaur
+    if (player_y==minotaur_y && player_x==minotaur_x) {
+        return YOU_LOSE;
+    }
     return KEEP_GOING;
 }
-
