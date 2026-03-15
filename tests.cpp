e585628a16f7104274c5351a52ed8a1716b01939
skippyjro-glitch <skippyjro@gmail.com>
2026-03-14 22:40:31 -0400

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -60,10 +60,10 @@ TEST_SUITE_BEGIN("Character tests");
         CHECK(calculate_newX(10, 'd')==11);
     }
     TEST_CASE("check 'w' makes new y-direction one more than original"){
-        CHECK(calculate_newX(10, 'w')==11);
+        CHECK(calculate_newY(10, 'w')==11);
     }
     TEST_CASE("check 's' makes new y-direction one less than original"){
-        CHECK(calculate_newX(10, 's')==9);
+        CHECK(calculate_newY(10, 's')==9);
     }
 // tests for charge_minotaur
 TEST_SUITE_END();
